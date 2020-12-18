46253966598a33e2ea1b2fcec0579f1ea64276ab
Darshan Navalakha <60502438+Darshan-Navalakha@users.noreply.github.com>
2020-12-18 13:05:45 +0530

    Update Calc.java
    New methode added

--- DemoApp/src/com/lti/model/Calc.java
+++ DemoApp/src/com/lti/model/Calc.java
@@ -8,4 +8,7 @@ public class Calc {
 	public void subs(int num1,int num2) {
 		System.out.println("Subttraction:" +(num1-num2));
 	}
+	public void subs1(int num1,int num2) {
+		System.out.println("Subttraction1:" +(num1-num2));
+	}
 }
